86dc8429d99c3de818d2eef6e12077bbf333d26d
aishakariz <aishaemin33@gmail.com>
2024-09-20 06:49:01 +0800

    Updated Aisha class

--- src/practice2/Aisha.java
+++ src/practice2/Aisha.java
@@ -1,4 +1,16 @@
 package practice2;
 
 public class Aisha {
+    public static void main(String[] args) {
+        System.out.println((Identify(12)));
+        System.out.println(Identify(13));
+    }
+
+    public static String Identify(int num){
+        if(num%2==0){
+            return"Even";
+        }else {
+            return"Odd";
+        }
+    }
 }
