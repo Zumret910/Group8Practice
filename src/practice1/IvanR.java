b6911ccd3b17ea4415319c19e0bd68594b00c6d4
Ivan_Cydeo <remezivan1@gmail.com>
2024-09-19 14:31:34 -0400

    oddOrEven method

--- src/practice1/IvanR.java
+++ src/practice1/IvanR.java
@@ -2,5 +2,22 @@ package practice1;
 
 public class IvanR {
 
+    public static void main(String[] args) {
 
+        oddOrEven(5);
+        oddOrEven(4);
+    }
+
+    public static void oddOrEven(int num) {
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
 }
+/*
+1️⃣  Numbers -- odd & even
+Write a method that can identify  a given number is even or odd.
+ */
