3a9ebb64e0cadcbfce7965d862af565117b0e355
Bayan Dzehverovic <bayanbaker@gmail.com>
2024-09-26 12:52:21 -0400

    Added class: FINRA Practice Problem

--- src/practice1/FINRA.java
+++ src/practice1/FINRA.java
@@ -18,7 +18,7 @@ public class FINRA {
 }
 
 /*
-FINRA:
+Assignment FINRA:
 Write a function that prints out the numbers from 1 to 30:
 - for numbers that are a multiple of 3, print "FIN" instead of the number;
 - for numbers that are a multiple of 5, print "RA" instead of the number;
