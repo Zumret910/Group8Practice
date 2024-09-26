8c2bccb6ce61bdd4261d08ba70ef899c9b67acf6
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-09-26 15:56:20 -0400

    update

---
+++ src/practice2/Hidir.java
@@ -0,0 +1,24 @@
+package src.practice2;
+
+public class Hidir {
+
+
+        public static void main(String[] args) {
+
+
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FINRA");
+                } else if (i % 3 == 0) {
+                    System.out.println("FIN");
+                } else if (i % 5 == 0) {
+                    System.out.println("RA");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
+
+
