ae3da421bd92ed5a2c877c4095d7d5f4310877ef
katth <katthleenf@gmail.com>
2024-09-28 03:05:35 -0400

    Added notes FINRA Practice class

--- src/practice2/Katth.java
+++ src/practice2/Katth.java
@@ -2,6 +2,7 @@ package practice2;
 
 public class Katth {
     public static void main(String[] args) {
+        System.out.println("----Use and add For Loop-----------");
         for(int i= 1; i <=30; i++) {
 
        System.out.println("----Check if numbers are divisible by 3 and 5----");
