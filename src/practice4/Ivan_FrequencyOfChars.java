8c6076beb1c54e18cadceec9f842a24774b8dca4
Ivan_Cydeo <remezivan1@gmail.com>
2024-11-07 10:48:31 -0500

    Practice 4 update

--- src/practice4/Ivan_FrequencyOfChars.java
+++ src/practice4/Ivan_FrequencyOfChars.java
@@ -1,10 +1,15 @@
 package practice4;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Ivan_FrequencyOfChars {
 
     public static void main(String[] args) {
 
         System.out.println("charFrequency(\"AAABBCDD\") = " + charFrequency("AAABBCDD"));
+
+        System.out.println("charFrequency_MAP(\"AAABBCDD\") = " + charFrequency_MAP("AAABBCDD"));
 
     }
     public static String charFrequency(String str) {
@@ -18,16 +23,31 @@ public class Ivan_FrequencyOfChars {
             for (int j = 0; j < str.length(); j++) {
 
                 if (str.charAt(i) == str.charAt(j)) {
-
                     freq++;
                 }
             }
             if ( ! result.contains("" + str.charAt(i))) {
-
                 result += "" + str.charAt(i) + freq;
             }
         }
         return result;
+    }
+
+    public static String charFrequency_MAP(String str) {
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            Character ch = str.charAt(i);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        return map.toString();
     }
 }
 /*
